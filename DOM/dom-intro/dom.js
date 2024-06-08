2463dc479edcd420dbfc655668cf548c099e0a45
Laizu Akram <laizuakram98@gmail.com>
2024-06-08 08:08:11 +0600

    basic event method

--- DOM/dom-intro/dom.js
+++ DOM/dom-intro/dom.js
@@ -5,44 +5,52 @@
 const sectionContainer = document.getElementById('section-container');
 
 //manually create and add a new section
-// const newSection = document.createElement('section');
-// const h1 = document.createElement('h1');
-// h1.innerText = 'Marketing Team';
+const newSection = document.createElement('section');
+const h1 = document.createElement('h1');
+h1.innerText = 'Marketing Team';
 
-// const newUl = document.createElement('ul');
-// const li = document.createElement('li');
-// const li2 = document.createElement('li');
-// const li3 = document.createElement('li');
-// const li4 = document.createElement('li');
+const newUl = document.createElement('ul');
+const li = document.createElement('li');
+const li2 = document.createElement('li');
+const li3 = document.createElement('li');
+const li4 = document.createElement('li');
 
-// li.innerText = 'Laizu';
-// li2.innerText = 'Nabiha';
-// li3.innerText = 'Arafat';
-// li4.innerText = 'Nasir';
+li.innerText = 'Laizu';
+li2.innerText = 'Nabiha';
+li3.innerText = 'Arafat';
+li4.innerText = 'Nasir';
 
-// newUl.appendChild(li);
-// newUl.appendChild(li2)
-// newUl.appendChild(li3)
-// newUl.appendChild(li4)
+newUl.appendChild(li);
+newUl.appendChild(li2)
+newUl.appendChild(li3)
+newUl.appendChild(li4)
 
-// newSection.appendChild(h1);
-// newSection.appendChild(newUl);
+newSection.appendChild(h1);
+newSection.appendChild(newUl);
 
-// sectionContainer.appendChild(newSection)
+sectionContainer.appendChild(newSection)
 
 //smart way to create and add new section
 const newSection2 = document.createElement('section');
- newSection2.innerHtml = `
-    <h1>my team</h1>
-    <ul>
-        <li>Laizu</li>
-        <li>Afra</li>
-        <li>Nasir</li>
-        <li>Najmin</li>
-    </ul>
-    
-`
-console.log(newSection2);
+
+ newSection2.innerHTML += `
+    <h1>New team</h1>
+        <ul>
+            <li>Afra</li>
+            <li>Nasir</li>
+            <li>Laizu</li>
+            <li>Video Editor</li>
+        </ul>
+ `
+ 
+sectionContainer.appendChild(newSection2);
+
+
+//remove child
+
+// const hideSection = document.getElementById('hide-section');
+// sectionContainer.removeChild(hideSection);
+
 
 //set color by using querysector
 const sectionAll = document.querySelectorAll('section');
