4a130a5d6c8a9839d8ffbd241beb340c200ff163
Laizu Akram <laizuakram98@gmail.com>
2024-06-10 09:01:02 +0600

    crete baap er bank ui with tailwind

---
+++ DOM/Baap-er-Bank/bank.js
@@ -0,0 +1,13 @@
+
+//login event
+
+document.getElementById('login-btn').addEventListener('click', ()=>{
+    //user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    
+    //user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    
+})
