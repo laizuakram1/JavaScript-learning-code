5e838395bb9c5886b416f20f77b38d816f2a7950
Laizu Akram <laizuakram98@gmail.com>
2024-06-13 13:09:15 +0600

    calculate all card with dom and create element

--- DOM/assignment-5/index.js
+++ DOM/assignment-5/index.js
@@ -1,9 +1,76 @@
 
-//add card event and set 
+//kitchen wear: first card 
 document.getElementById('kitchen-hata').addEventListener('click', () =>{
     
     setList('selected-list', 'hata-name');
     setPrice('hata-price', 'total-price', 'discount-price', 'total');
+   
+})
+//kitchen wear: second card 
+document.getElementById('cutting-plate').addEventListener('click', () =>{
+    
+    setList('selected-list', 'cutting-plate-name');
+    setPrice('cutting-plate-price', 'total-price', 'discount-price', 'total');
+   
+})
+//kitchen wear: thrird card 
+document.getElementById('curry-coocker').addEventListener('click', () =>{
+    
+    setList('selected-list', 'curry-coocker-name');
+    setPrice('curry-coocker-price', 'total-price', 'discount-price', 'total');
+   
+})
+
+/**************************************************/
+
+//sports wear fn
+
+//sports wear: sports cap card 
+document.getElementById('sports-cap').addEventListener('click', () =>{
+    
+    setList('selected-list', 'sports-cap-name');
+    setPrice('sports-cap-price', 'total-price', 'discount-price', 'total');
+   
+})
+//sports wear: sports jersey card 
+document.getElementById('sports-jersey').addEventListener('click', () =>{
+    
+    setList('selected-list', 'sports-jersey-name');
+    setPrice('sports-jersey-price', 'total-price', 'discount-price', 'total');
+   
+})
+//sports wear: sports shoe card 
+document.getElementById('sports-shoe').addEventListener('click', () =>{
+    
+    setList('selected-list', 'sports-shoe-name');
+    setPrice('sports-shoe-price', 'total-price', 'discount-price', 'total');
+   
+})
+
+
+/****************************************************/
+
+//furniture wear
+
+//foaming chair
+document.getElementById('foaming-chair').addEventListener('click', () =>{
+    
+    setList('selected-list', 'foaming-chair-name');
+    setPrice('foaming-chair-price', 'total-price', 'discount-price', 'total');
+   
+})
+//folding chair
+document.getElementById('folding-chair').addEventListener('click', () =>{
+    
+    setList('selected-list', 'folding-chair-name');
+    setPrice('folding-chair-price', 'total-price', 'discount-price', 'total');
+   
+})
+//foaming chair
+document.getElementById('single-sofa').addEventListener('click', () =>{
+    
+    setList('selected-list', 'single-sofa-name');
+    setPrice('single-sofa-price', 'total-price', 'discount-price', 'total');
    
 })
 
