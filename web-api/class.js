452166ae0b3cb3edf864a2ccef6f8bc646129eeb
Laizu Akram <laizuakram98@gmail.com>
2024-06-22 19:43:12 +0600

    add class constructor

---
+++ web-api/class.js
@@ -0,0 +1,17 @@
+
+//javascript class have property, method
+
+class Person{
+    constructor(name, address, occupation){
+        this.name = name;
+        this.address = address;
+        this.occupation = occupation;
+        
+    }
+    properties = 'home, car and bike'
+
+    
+}
+
+const newPerson = new Person('laizu', 'bangladesh', 'web developer');
+console.log(newPerson);
