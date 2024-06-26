63d74312273420cf33e7ab274c3bdb12159a4ed7
Laizu Akram <laizuakram98@gmail.com>
2024-06-26 22:40:43 +0600

    json strigigy and parse

--- web-api/json-api/json.js
+++ web-api/json-api/json.js
@@ -1,5 +1,23 @@
 
-const data = {
-    name: 'laizu',
-    address: 'Bangladesh' 
+//JSON = JavaScript Object Notation
+
+
+const myShop = {
+    name: 'laizu shop',
+    address: {
+        city: 'dhaka',
+        country: 'bangladesh'
+    },
+    products: ['polo-tshirt', 't-shirt', 'loofer', 'watch'],
+    serviceArea: 'inside of Bangladesh',
+    isOpen: true,
+    isNew: false,
+    availableEMI: 'yes'
 }
+
+//JOSN Stringify = object to string
+const jsonObject = JSON.stringify(myShop);
+
+//JSON Parse = string(JSON data) to object
+const parseData = JSON.parse(jsonObject)
+console.log(parseData);
