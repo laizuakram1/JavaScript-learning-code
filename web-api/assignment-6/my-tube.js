34ef3fbedcb4224310e17e046cea8fa37436e394
Laizu Akram <laizuakram98@gmail.com>
2024-07-16 09:32:31 +0600

    load specific data

--- web-api/assignment-6/my-tube.js
+++ web-api/assignment-6/my-tube.js
@@ -4,20 +4,47 @@ const loadData = async ()=>{
     const res = await fetch(url)
     const result = await res.json()
     const data = result.data;
-    
+   
     displayData(data);
 }
 loadData();
 
+const musicData = async () =>{
+    const url = 'https://openapi.programming-hero.com/api/videos/category/1001'
+    const res = await fetch(url)
+    const result = await res.json()
+    const musicData = result.data;
+    
+    displayData(musicData)
+}
+
+const comedyData = async () =>{
+    const url = 'https://openapi.programming-hero.com/api/videos/category/1003'
+    const res = await fetch(url)
+    const result = await res.json()
+    const comedyData = result.data;
+    
+    displayData(comedyData)
+}
+
+
 const displayData = (data)=>{
+    
     const displayContainer = document.getElementById('display')
+    displayContainer.innerText = ''
     
     data.map((ele) => {
-        console.log(ele)
+        // const views = ele.others.views
+        // const viewsArr = []
+        // viewsArr.push(views);
+        // viewsArr.map((view) => console.log(view.slice(0, 3)))
+
         const card = document.createElement('div')
+        card.classList.add('card')
         card.innerHTML = `
             <img src='${ele.thumbnail}'/>
-            <h2>${ele.title}</h2>
+            
+            <h4>${ele.title}</h4>
         `
         displayContainer.appendChild(card)
     })
